417bdc434efd5611f4fe937c1a4dc527c8f6a3af
Carmen Morata <cmoratagarcia@gmail.com>
2024-11-14 12:17:44 -0800

    Log the response to the console

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function fetchWeatherData(location) {
   fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=ZBZPAPPSJEVGRLW8AU8DAEBCT`,
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=ZBZPAPPSJEVGRLW8AU8DAEBCT&contentType=json`,
     { mode: "cors" }
-  );
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      console.log(response);
+    });
 }
+
+fetchWeatherData("Madrid");
