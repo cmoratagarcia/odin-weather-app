91db6b64c00549b2fed061f3f3b7f33d7d45001c
Carmen Morata <cmoratagarcia@gmail.com>
2024-11-14 12:22:10 -0800

    Update logged data to only what the app requires

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ function fetchWeatherData(location) {
     .then((response) => {
       return response.json();
     })
+    //Write the functions that process the JSON data you’re getting from the API and return an object with only the data you require for your app.
     .then((response) => {
-      console.log(response);
+      console.log(response.currentConditions.conditions);
+      console.log(response.currentConditions.temp);
     });
 }
 
